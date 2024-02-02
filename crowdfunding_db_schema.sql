025e38c717ca5d33bcee51396cad5a8293b80908
thee.xiong <yang.t17@gmail.com>
2024-02-01 23:03:57 -0600

    added character max

--- crowdfunding_db_schema.sql
+++ crowdfunding_db_schema.sql
@@ -3,16 +3,16 @@
 
 
 CREATE TABLE "category" (
-    "category_id" varchar   NOT NULL UNIQUE,
-    "category" varchar   NOT NULL UNIQUE,
+    "category_id" varchar(30)   NOT NULL UNIQUE,
+    "category" varchar(30)   NOT NULL UNIQUE,
     CONSTRAINT "pk_category" PRIMARY KEY (
         "category_id"
      )
 );
 
 CREATE TABLE "subcategory" (
-    "subcategory_id" varchar   NOT NULL UNIQUE,
-    "subcategory" varchar   NOT NULL UNIQUE,
+    "subcategory_id" varchar(30)   NOT NULL UNIQUE,
+    "subcategory" varchar(30)   NOT NULL UNIQUE,
     CONSTRAINT "pk_subcategory" PRIMARY KEY (
         "subcategory_id"
      )
@@ -20,8 +20,8 @@ CREATE TABLE "subcategory" (
 
 CREATE TABLE "contacts" (
     "contact_id" int   NOT NULL UNIQUE,
-    "first_name" varchar   NOT NULL,
-    "last_name" varchar   NOT NULL,
+    "first_name" varchar(30)   NOT NULL,
+    "last_name" varchar(30)   NOT NULL,
     "email" varchar   NOT NULL,
     CONSTRAINT "pk_contacts" PRIMARY KEY (
         "contact_id"
@@ -35,14 +35,14 @@ CREATE TABLE "campaign" (
     "description" varchar   NOT NULL,
     "goal" float   NOT NULL,
     "pledged" float   NOT NULL,
-    "outcome" varchar   NOT NULL,
+    "outcome" varchar(10)   NOT NULL,
     "backers_count" int   NOT NULL,
-    "country" varchar   NOT NULL,
-    "currency" varchar   NOT NULL,
+    "country" varchar(5)   NOT NULL,
+    "currency" varchar(5)   NOT NULL,
     "launched_date" date   NOT NULL,
     "end_date" date   NOT NULL,
-    "category_id" varchar   NOT NULL,
-    "subcategory_id" varchar   NOT NULL,
+    "category_id" varchar(10)   NOT NULL,
+    "subcategory_id" varchar(10)   NOT NULL,
     CONSTRAINT "pk_campaign" PRIMARY KEY (
         "cf_id"
      )
